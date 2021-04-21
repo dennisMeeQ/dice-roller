67363a365d281976efa4204b823ec0c7d1f321a0
Dennis Stötzel <github@meeq.de>
2021-04-21 21:41:48 +0200

    eslint/prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'prettier'],
+  extends: ['airbnb', 'prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': 'warn',
+    'no-console': 'off',
+    'no-use-before-define': 'off',
+    'no-plusplus': 'off',
+    'react/jsx-filename-extension': 0,
+    'react/jsx-boolean-value': 'off',
+    'react/prop-types': [0],
+    'react/no-danger': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/no-unused-prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/react-in-jsx-scope': 0,
+  },
+  overrides: [
+    {
+      files: ['*.test.js', '*.spec.js'],
+      rules: {
+        'import/named': 'off',
+        'import/no-unresolved': 'off',
+      },
+    },
+  ],
+};
